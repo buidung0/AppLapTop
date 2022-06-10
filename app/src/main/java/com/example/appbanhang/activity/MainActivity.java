3bb68ad20635533abc50b8cd1c7e4e6b7ca89cca
Justblue0312 <trao0312@gmail.com>
2022-06-10 12:21:35 +0700

    first commit

--- app/src/main/java/com/example/appbanhang/activity/MainActivity.java
+++ app/src/main/java/com/example/appbanhang/activity/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(dienthoai);
                         break;
                     case 2:
-                        Intent laptop = new Intent(getApplicationContext(), MacBookActivity.class);
+                        Intent laptop = new Intent(getApplicationContext(), LapTopActivity.class);
                         laptop.putExtra("loai",2);
                         startActivity(laptop);
                         break;
